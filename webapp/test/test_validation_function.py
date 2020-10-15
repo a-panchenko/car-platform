fbe3f5d6a16689c6cf3eb2452531c7cf26c3cd8e
Mariia Vasyleha <maria.vasyleh@gmail.com>
2020-10-15 18:01:01 +0200

    made changes to test_validation_function

--- webapp/test/test_validation_function.py
+++ webapp/test/test_validation_function.py
@@ -1,8 +1,11 @@
 import unittest
 import datetime
+
+from marshmallow.exceptions import ValidationError
+
 from ..webapp_platform.models import CarListing, CarListingSchema, CarAttributes
 
-class TestValFunction(unittest.TestCase):
+class TestValidationFunction(unittest.TestCase):
     expected_value = CarListing("1234", datetime.date(2019, 10, 1), "001", "0542271836", "USA",
                                 CarAttributes("345", "A8790", "gas", "automatic", "1FTFW1EF8EFA71429",
                                               4000, 1990, "sedan", "gray"), 900)
@@ -16,50 +19,53 @@ class TestValFunction(unittest.TestCase):
         car_attributes_values = [manufacturer_id, model_id, fuel, transmission, vin, mileage, year, type, color]
         car_attributes_dict = dict(zip(car_attributes_keys, car_attributes_values))
         list_values = [id, date, author_id, author_phone, state, car_attributes_dict, customer_price]
-        dict1 = dict(zip(list_keys, list_values))
-        return dict1
+        return dict(zip(list_keys, list_values))
 
-    def test_val_function_ok(self):
+    def test_validation_function_with_valid_data(self):
         value = self.create_value()
         actual_value = CarListingSchema().load(value)
         self.assertTrue(isinstance(actual_value, CarListing))
         self.assertTrue(self.expected_value == actual_value)
 
-    def test_val_date(self):
+    def test_validation_invalid_format_date(self):
         value = self.create_value(date ="2019/10/1")
-        actual_value = CarListingSchema().load(value)
-        self.assertTrue(isinstance(actual_value, CarListing))
-        self.assertTrue(self.expected_value == actual_value)
+        with self.assertRaises(ValidationError):
+            CarListingSchema().load(value)
 
-    def test_val_author_id(self):
+    def test_validation_invalid_format_day(self):
+        value = self.create_value(date ="2019-10-33")
+        with self.assertRaises(ValidationError):
+            CarListingSchema().load(value)
+
+    def test_validation_date_invalid_year(self):
+        value = self.create_value(date ="2029-10-1")
+        with self.assertRaises(ValueError):
+            CarListingSchema().load(value)
+
+    def test_validation_invalid_author_id(self):
         value = self.create_value(author_id = 1)
-        actual_value = CarListingSchema().load(value)
-        self.assertTrue(isinstance(actual_value, CarListing))
-        self.assertTrue(self.expected_value == actual_value)
+        with self.assertRaises(ValidationError):
+            CarListingSchema().load(value)
 
-    def test_val_manufacturer_id(self):
+    def test_validation_invalid_manufacturer_id(self):
         value = self.create_value(manufacturer_id = 111)
-        actual_value = CarListingSchema().load(value)
-        self.assertTrue(isinstance(actual_value, CarListing))
-        self.assertTrue(self.expected_value == actual_value)
+        with self.assertRaises(ValidationError):
+            CarListingSchema().load(value)
 
-    def test_val_year(self):
+    def test_validation_invalid_year(self):
         value = self.create_value(year = 2029)
-        actual_value = CarListingSchema().load(value)
-        self.assertTrue(isinstance(actual_value, CarListing))
-        self.assertTrue(self.expected_value == actual_value)
+        with self.assertRaises(ValidationError):
+            CarListingSchema().load(value)
 
-    def test_val_fuel(self):
+    def test_validation_invalid_fuel(self):
         value = self.create_value(fuel = 'not_gas')
-        actual_value = CarListingSchema().load(value)
-        self.assertTrue(isinstance(actual_value, CarListing))
-        self.assertTrue(self.expected_value == actual_value)
+        with self.assertRaises(ValidationError):
+            CarListingSchema().load(value)
 
-    def test_val_type(self):
+    def test_validation_invalid_type(self):
         value = self.create_value(type = 'sport')
-        actual_value = CarListingSchema().load(value)
-        self.assertTrue(isinstance(actual_value, CarListing))
-        self.assertTrue(self.expected_value == actual_value)
+        with self.assertRaises(ValidationError):
+            CarListingSchema().load(value)
 
 if __name__ == '__main__':
     unittest.main()
